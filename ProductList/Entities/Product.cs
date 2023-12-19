4252cc0ca08e336b3fa3ba3c771205868337a7e7
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-12-19 20:06:52 -0300

    Finalização e testes

---
+++ ProductList/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ProductList.Entities {
+    class Product {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price) {
+            Name = name;
+            Price = price;
+        }
+    }
+}
--- ProductList/Program.cs
+++ ProductList/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using ProductList.Entities;
+
+namespace ProductList;
+
+internal class Program {
+    static void Main(string[] args) {
+        Console.Write("Enter full file path: ");
+        string path = Console.ReadLine();
+
+        List<Product> list = new List<Product>();
+
+        using(StreamReader sr = File.OpenText(path)) {
+            while(!sr.EndOfStream) {
+                string[] fields = sr.ReadLine().Split(',');
+                string name = fields[0];
+                double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                list.Add(new Product(name, price));
+            }
+        }
+
+        var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+        Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+        var names = list.Where(p => p.Price <= avg).OrderByDescending(p => p.Name).Select(p => p.Name);
+
+        foreach(string name in names) {
+            Console.WriteLine(name);
+        }
+    }
+}
